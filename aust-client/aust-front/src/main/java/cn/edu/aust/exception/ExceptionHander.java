785a7699b888a30c982013013a2c9a12c34a1681
quding <li.niu@duitang.com>
2017-03-20 15:16:50 +0800

    异常标准化

--- aust-client/aust-front/src/main/java/cn/edu/aust/exception/ExceptionHander.java
+++ aust-client/aust-front/src/main/java/cn/edu/aust/exception/ExceptionHander.java
@@ -1,25 +1,20 @@
 package cn.edu.aust.exception;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.HandlerExecutionChain;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import cn.edu.aust.common.constant.PosCode;
+import cn.edu.aust.common.entity.ResultVO;
+import cn.edu.aust.common.util.CgiHelper;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -28,33 +23,51 @@ import lombok.extern.slf4j.Slf4j;
  * @date 2016/9/4
  */
 @Slf4j
-public class ExceptionHander implements HandlerExceptionResolver{
+@ControllerAdvice
+public class ExceptionHander {
 
-    @Override
-    public ModelAndView resolveException(HttpServletRequest request,
-                                         HttpServletResponse response,
-                                         Object o,
-                                         Exception exception) {
-        String requestType = request.getHeader("x-requested-with");
-        ModelAndView modelAndView = new ModelAndView();
-        Throwable throwable = exception.getCause();
-        modelAndView.addObject("errorMessage", throwable);
-        log.error("捕捉到异常", exception);
-        //为ajax请求
-        if (StringUtils.equals(requestType,"XMLHttpRequest")){
-            JSONObject result = new JSONObject();
-            result.put("status",96);
-            result.put("msg",exception.getMessage());
-            log.info(result.toJSONString());
-            try {
-                response.setHeader("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
-                response.getWriter().write(JSON.toJSONString(result));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }else {
-            modelAndView.setViewName("error");
-        }
-        return modelAndView;
+  /**
+   * 全局异常处理
+   *
+   * @param request  请求
+   * @param response 返回
+   * @param ex       异常
+   */
+  @ExceptionHandler(value = Exception.class)
+  @ResponseStatus(HttpStatus.OK)//前端不支持处理非200的异常
+  @ResponseBody
+  public Object exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) {
+    //判断是否为ajax请求
+    String xRequested = CgiHelper.getHeader("x-requested-with",null,request);
+    if (StringUtils.equalsIgnoreCase(xRequested,"XMLHttpRequest")){
+      return handlerAjax(ex);
     }
+
+    //重定向到错误页面
+    redirect("/error",HttpStatus.NOT_FOUND,response);
+    return null;
+  }
+
+  /**
+   * 处理ajax异常
+   * @param ex 异常
+   * @return json异常信息
+   */
+  private ResultVO handlerAjax(Exception ex){
+    ResultVO resultVO = new ResultVO();
+    return resultVO.buildWithMsgAndStatus(PosCode.NO_PRIVILEGE,ex.getMessage());
+  }
+
+  /**
+   * 重定向到错误页面
+   * @param url 链接
+   */
+  private void redirect(String url,HttpStatus status, HttpServletResponse response){
+    try {
+      response.setStatus(status.value());
+      response.sendRedirect(url);
+    } catch (IOException e) {
+      log.error("redirect fail,e:{}",e);
+    }
+  }
 }
